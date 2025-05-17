40df1f7a900ddfc9c00338f21c0fc13e0839bd84
DakshBeep <102212500+DakshBeep@users.noreply.github.com>
2025-05-16 21:50:14 -0700

    Fix forward reference for User

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ JWT_SECRET = "budgeteer-secret"
 JWT_ALGORITHM = "HS256"
 
 
-def get_current_user(authorization: str = Header(None)) -> User:
+def get_current_user(authorization: str = Header(None)) -> "User":
     if not authorization:
         raise HTTPException(status_code=401, detail="Missing token")
     token = authorization.replace("Bearer", "").strip()
