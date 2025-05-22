d4a51d4f99869d07dc31ec1e5cd8f1b255a8009d
DakshBeep <102212500+DakshBeep@users.noreply.github.com>
2025-05-22 12:59:55 -0700

    Add HTML template and configure HtmlWebpackPlugin

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,5 +14,8 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' })
+  ],
   devServer: { static: './dist' }
 };
